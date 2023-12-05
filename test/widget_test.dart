b1af70dc629db6cf10bf7bd3d7f21e0b0c13bfcd
naroitant <naro29rus@gmail.com>
2023-12-06 02:06:27 +0300

    Added login and password authentication.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,17 +4,4 @@ import 'package:shimmer/main.dart';
 
 import 'package:y/main.dart';
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
-
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
-}
+void main() { }
